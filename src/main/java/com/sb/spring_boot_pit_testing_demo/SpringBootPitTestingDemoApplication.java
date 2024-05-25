646b2f6ea905da3d7d8273d9f302d8428f479e0c
SumitBhawsar <sumib.8.6@gmail.com>
2024-05-25 11:44:15 +0530

    initial commit with project structure

---
+++ src/main/java/com/sb/spring_boot_pit_testing_demo/SpringBootPitTestingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sb.spring_boot_pit_testing_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPitTestingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPitTestingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/spring_boot_pit_testing_demo/SpringBootPitTestingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.spring_boot_pit_testing_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPitTestingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
